30f258d36a410acdbe05d7d064ff5f7bf5632a0d
ReachOutToHardik <hardik.connect14@gmail.com>
2026-06-17 12:57:51 +0530

    fix: revert previous AWS hacks, add middleware to redirect root domain to www

--- next.config.mjs
+++ next.config.mjs
@@ -13,30 +13,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/services/credit-rectification-in-:city',
-        destination: '/services/credit-rectification/:city',
-        permanent: true,
-      },
-      {
-        source: '/credit-rectification-in-:city',
-        destination: '/services/credit-rectification/:city',
-        permanent: true,
-      },
-      {
-        source: '/services/credit-rectification-in:city', // Catches merged like 'inudaipur'
-        destination: '/services/credit-rectification/:city',
-        permanent: true,
-      },
-      {
-        source: '/credit-rectification-in:city', // Catches merged like 'inudaipur'
-        destination: '/services/credit-rectification/:city',
-        permanent: true,
-      }
-    ]
-  }
 }
 
 export default nextConfig
