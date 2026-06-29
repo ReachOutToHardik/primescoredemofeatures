967306474e98b838c5cb9ca1efcf0a48b7c5f1d1
ReachOutToHardik <hardik.connect14@gmail.com>
2026-06-29 16:48:02 +0530

    feat: replace vector svg map on about page with new india network design graphic

--- src/components/ui/IndiaMap.tsx
+++ src/components/ui/IndiaMap.tsx
@@ -17,22 +17,19 @@ export default function IndiaMap() {
         />
       </div>
 
-      {/* Directly render the SVG image file */}
+      {/* Directly render the custom network graphics map illustration */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/india-map/india.svg"
-        alt="Map of India"
+        src="/india-map/india-map-network.png"
+        alt="Primescore India Wide Support Network Map"
         width={540}
-        height={610}
-        className="relative z-10 w-full h-auto opacity-75"
-        style={{
-          filter: 'hue-rotate(200deg) saturate(1.2) brightness(0.95)',
-        }}
+        height={540}
+        className="relative z-10 w-full h-auto drop-shadow-[0_10px_30px_rgba(0,0,0,0.05)]"
         draggable={false}
       />
 
       {/* "India-wide operations" label */}
-      <div className="flex items-center justify-center gap-2 mt-3">
+      <div className="flex items-center justify-center gap-2 mt-5">
         <div className="h-px w-8 bg-brandNavy/20" />
         <span className="text-[10px] font-bold uppercase tracking-[0.25em] text-brandNavy/40">
           India-wide operations
